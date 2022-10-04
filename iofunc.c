adedadb88485d65bf855d3293161be7cfe90f17b
przekaska <kacper.blaszkowski.12@gmail.com>
2022-10-04 18:44:40 +0200

    Created iofunc.c
    Created iofunc.c that will contain some necessary input/output functions. Created 'printmsg' function that prints message in three different ways, it works but still needs some improvement. Created also 'take_input' function that works as a normal getstr() function, but it print input echo in three diffrent ways, simillar to 'printmsg' function.

--- iofunc.c
+++ iofunc.c
@@ -1,5 +1,6 @@
 #include <curses.h>
 #include <string.h>
+#include <stdlib.h>
 
 #ifdef __linux__
     #include <unistd.h>
@@ -12,6 +13,13 @@
 #define IOLEFT 0
 #define IOCENT 1
 #define IORIGH 2
+
+void I_LEFT(WINDOW *win, int y, int x, int size, char *input);
+void I_CENT(WINDOW *win, int y, int x, int size, char *input);
+void I_RIGH(WINDOW *win, int y, int x, int size, char *input);
+
+
+/* ############## OUTPUT ############## */
 
 void printmsg(WINDOW *win, int y, int x, const char *msg, const int option){
     int msg_len;
@@ -36,7 +44,7 @@ void printmsg(WINDOW *win, int y, int x, const char *msg, const int option){
 
         refresh();
 
-        /*Between printing every character, take a break for a nice efect of printing*/
+        /* After printing every character, take a break for a nice efect of printing */
         #ifdef __linux__
             usleep(IODELAY);
         #elif _WIN32
@@ -44,3 +52,52 @@ void printmsg(WINDOW *win, int y, int x, const char *msg, const int option){
         #endif
     }
 }
+
+
+
+
+
+/* ############## INPUT ############## */
+
+/* This function can handle user input echo in three different ways:
+* Option 1 (I_LEFT): Echo input from the left to the right (like usually done)
+* Option 2 (I_CENT): Echo displayed that the center of the string stays in the defined position
+* Option 3 (I_RIGH): Echo displayed from the right to the left. Opposite to option 1 */
+
+/* NOTE: In every option input is printed char by char, because this way provide much simpler
+   implementation of handling input echoing.  */
+
+char* take_input(WINDOW *win, int y, int x, int size, void (*option)(WINDOW*, int, int, int, char*)){
+    char* input;
+    input = calloc(size, sizeof(int));          // Maximum size has to be predefined, because of performance
+
+    option(win, y, x, size, input);            
+    
+    return input;
+}
+
+void I_LEFT(WINDOW *win, int y, int x, int size, char *input){
+    wmove(win, y, x);
+        for(int i = 0; (input[i] = getch()) != 10; i++)
+            if(i < size)
+                waddch(win, input[i]);
+}
+
+
+void I_CENT(WINDOW *win, int y, int x, int size, char *input){
+    for(int i = 0; (input[i] = getch()) != 10; i++)
+        if(i < size){
+            wmove(win, y, x - i/2);
+            for (int j = 0; j <= i; j++)
+                waddch(win, input[j]);
+        }
+}
+
+
+void I_RIGH(WINDOW *win, int y, int x, int size, char *input){
+    for (int i = 0; (input[i] = getch()) != 10; i++)
+        if (i < size)
+            for (int j = 0; j <= i; j++)
+                mvwaddch(win, y, x - i + j, input[j]);              // use mvwaddch insted of waddch, because the 
+}                                                                   // beggining of the echo needs to be moved every
+                                                                    // time user enters character
