323339a85d6b8de2c1d282252f4b5901d82eeee3
przekaska <kacper.blaszkowski.12@gmail.com>
2022-10-07 07:59:42 +0200

    Created setup_curses.c
    Created setup_curses.c that contains only function that set up curses attributes

---
+++ setup_curses.c
@@ -0,0 +1,19 @@
+#ifndef _AN_SET_CURS_C
+#define _AN_SET_CURS_C
+
+#include <curses.h>
+
+void init_curses(){
+    initscr();
+    curs_set(0);
+    noecho();
+    keypad(stdscr, TRUE);
+}
+
+void end_curses(){
+    getch();
+    endwin();
+}
+
+#endif
+
