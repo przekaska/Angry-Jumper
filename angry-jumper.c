ad95f77aaa7512950082e5f9b98adbfb0831aa03
przekaska <kacper.blaszkowski.12@gmail.com>
2022-10-07 08:04:28 +0200

    Added 'init_curses()' that sets up curses parameters and 'end_curses()', which clean up after end_curses(). Made call to 'main_menu' which creates main menu from menu.c

--- angry-jumper.c
+++ angry-jumper.c
@@ -1,5 +1,21 @@
+#ifndef _AN_ANGRYJUMPER_C
+#define _AN_ANGRYJUMPER_C
+
 #include <curses.h>
+#include <stdlib.h>
+
+#include "iofunc.c"
+#include "menu.c"
+#include "setup_curses.c"
 
 int main() {
-    
+    char* username;
+
+    init_curses();              // Initialize curses and set up basic curses parameters
+
+    main_menu(username);z
+
+    end_curses();
 }
+
+#endif
