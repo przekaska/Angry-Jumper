7eba04f6e5557f17aeca7f2a4b816d188531834a
przekaska <kacper.blaszkowski.12@gmail.com>
2022-10-07 08:50:05 +0200

    Moved source files to 'src' folder and added 'background.c' and 'player.c'

---
+++ src/background.c
@@ -0,0 +1,8 @@
+#ifndef _AN_BACKGROUND_C
+#define _AN_BACKGROUND_C
+
+
+
+
+
+#endif
---
+++ src/player.c
@@ -0,0 +1,14 @@
+#ifndef _AN_PLAYER_C
+#define _AN_PLAYER_C
+
+#include <curses.h>
+
+struct Player{
+    int y, x;
+    int speed;
+    int health;    
+};
+
+
+
+#endif
