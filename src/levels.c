96555857d96f7e220c7364df87194f286563aac2
przekaska <kacper.blaszkowski.12@gmail.com>
2022-10-10 21:19:43 +0200

    Created print_level() function
    Created function that shows scene on the screen. As a parameter it takes pointer to the buffer that contains level data (string)

--- src/levels.c
+++ src/levels.c
@@ -3,10 +3,12 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <curses.h>
 
 #include "setup_curses.c"
 
-#define SCENE_SIZE 1600
+#define SCENE_LINES 24 
+#define SCENE_COLS 91   // last column for new line character
 
 #define LV1_SCENES_NUMBER 1
 
@@ -14,7 +16,6 @@
 void read_level(const char *filename, char* buffer){
     FILE *file = fopen(filename, "r");
     int c;
-    char buff[5000];
 
     if(NULL == file){
         printf("Cannot find file: %s", filename);
@@ -24,11 +25,14 @@ void read_level(const char *filename, char* buffer){
 
     for(int i = 0; (c = getc(file)) != EOF; i++)
         buffer[i] = c;
-
 }
 
 
-
+void print_level(char* level){
+    move(0, 0);
+    for(int i = 0; i < SCENE_COLS * SCENE_LINES * LV1_SCENES_NUMBER; i++)
+        addch(level[i]);
+}
 
 
 #endif
