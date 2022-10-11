13dbbfc2bf70ba63500a00cb74983739ef9e0130
przekaska <kacper.blaszkowski.12@gmail.com>
2022-10-11 16:36:42 +0200

    Created 'player.c' that will contain player functionality

---
+++ src/player.c
@@ -0,0 +1,19 @@
+#ifndef _AN_PLAYER_C
+#define _AN_PLAYER_C
+
+#define DEFAULT_Y 0
+#define DEFAULT_X 0
+#define DEFAULT_HEALTH 10
+
+struct Player{
+    int y, x;
+    int health;
+};
+
+
+void init_player(){
+    
+}
+
+
+#endif
