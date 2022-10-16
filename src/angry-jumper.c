e727dda597913fafba76313ca3500931ce670879
przekaska <kacper.blaszkowski.12@gmail.com>
2022-10-16 11:32:52 +0200

    Completely changed game idea

--- src/angry-jumper.c
+++ src/angry-jumper.c
@@ -3,31 +3,70 @@
 
 #include <curses.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "iofunc.c"
 #include "menu.c"
 #include "setup_curses.c"
-#include "levels.c"
-#include "objects.c"
+#include "player.c"
+
 
 int main(){
     char* username;
-    int control;
-    char level1[SCENE_COLS * SCENE_LINES * LV1_SCENES_NUMBER];
+
+                    //  y1  x1   w1  y2 x2 w2 y3 x3 w3 y4 x4 w4 y5 x5 w5
+    int platforms[16] = {5, 50, 10, 15, 130, 4, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0};                 
+    char key;
+
+    int move_iterator = 0;
+    int player_x = 0;
+    int position_buffer = 0;
 
     init_curses();              // Initialize curses and set up basic curses parameters
     
-    if(getmaxy(stdscr) <= SCENE_LINES || getmaxx(stdscr) <= SCENE_COLS){
-        printf("Your terminal is to small. Resize window");
-        end_curses();
-        exit(-1);
-    }
+    // if(getmaxy(stdscr) <= 20 || getmaxx(stdscr) <= 80){
+        // printf("Your terminal is to small. Resize window");
+        // end_curses();
+        // exit(-1);
+    // }
 
     /* TODO: Make welcome screen*/
-    main_menu(username);        // from menu.c   
-    read_level("levels/level1.txt", level1);
-    print_level(level1);
-    add_object(stdscr, 0, 0, OB_CLOUD5);
+    main_menu(username);        // from menu.c
+    
+    struct Player *player = init_player();
+    player->move = player->fall;
+
+    nodelay(stdscr, TRUE);
+    while((key = getch()) != 10){
+        
+        player->y += player->move[move_iterator];
+
+        if(key == 3){
+            player->move = player->jump;
+            move_iterator = 0;
+        }
+
+
+        if(move_iterator == 31){
+            move_iterator = 0;
+            player->move = player->idle;
+        }
+
+
+        mvaddstr(STD_Y - position_buffer, STD_X, " ");    
+        mvaddstr(STD_Y - player->y, STD_X, player->skin);
+
+        for(int i = 0; platforms[i] != -1; i += 3){
+            mvaddch(STD_Y - platforms[i], platforms[i + 1] - player_x, 'Z');
+            mvaddch(STD_Y - platforms[i], platforms[i + 1] - player_x + platforms[i + 2], ' ');
+        }
+
+        usleep(20000);
+        move_iterator++;
+        player_x++;
+        position_buffer = player->y;
+    }
+
 
     refresh();
 
--- src/player.c
+++ src/player.c
@@ -1,19 +1,48 @@
 #ifndef _AN_PLAYER_C
 #define _AN_PLAYER_C
 
-#define DEFAULT_Y 0
-#define DEFAULT_X 0
-#define DEFAULT_HEALTH 10
+#include <curses.h>
+
+#define STD_Y 30
+#define STD_X 10
+
 
 struct Player{
-    int y, x;
-    int health;
+    char* skin;
+    int y;
+
+    /* MOVES */
+    char *idle;
+    char *jump; 
+    char *fall;
+
+    char* move;
 };
 
 
-void init_player(){
+struct Player *init_player(){
+    static char idle[32] = {};
+
+    static char jump[32] = {1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0,
+                     0, 0, -1, 0, 0, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1};
+
+    static char fall[32] = {0, 0, -1, 0, 0, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1,
+                  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
     
+
+    static struct Player player;
+    player.skin = "O";
+    player.y = STD_Y;
+    player.idle = idle;
+    player.jump = jump;
+    player.fall = fall;
+
+
+    return &player;
 }
 
+void player_jump(int y){
+
+}
 
 #endif
